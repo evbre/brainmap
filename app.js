b12cb927f5b27f21449d18bab7d3f4c669120109
Eve Brettell <Eve Brettell>
2016-04-09 22:17:46 -0400

    almost done

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(__dirname + '/public')); 
+
+app.use('/img', express.static(__dirname + '/public')); 
+
 // --- routes --- //
+
 app.get("/",function(req,res){
 	res.render("home");
 });
@@ -15,19 +20,19 @@ app.get("/map1",function(req,res){
 });
 
 app.get("/map2",function(req,res){
-	res.render("map1");
+	res.render("map2");
 });
 
 app.get("/map3",function(req,res){
-	res.render("map1");
+	res.render("map3");
 });
 
 app.get("/map4",function(req,res){
-	res.render("map1");
+	res.render("map4");
 });
 
 app.get("/map5",function(req,res){
-	res.render("map1");
+	res.render("map5");
 });
 
 app.listen(3000, function(){
